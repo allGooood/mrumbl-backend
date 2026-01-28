782276c040fa1809300c0682a3646fc290820037
minkyeong <ming920331@gmail.com>
2026-01-28 13:53:58 +0900

    feat(security): add CORS setting

--- src/main/java/com/mrumbl/backend/common/config/SecurityConfig.java
+++ src/main/java/com/mrumbl/backend/common/config/SecurityConfig.java
@@ -16,6 +16,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.http.HttpMethod.*;
 
